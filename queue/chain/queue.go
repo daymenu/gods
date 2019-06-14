d9585815bbbdee215643f083c0293323cc076170
韩剑 <hanjian@xgo.cn>
2019-06-14 18:27:38 +0800

    queue

--- queue/chain/queue.go
+++ queue/chain/queue.go
@@ -1,22 +1,28 @@
 package chain
 
+import (
+	"fmt"
+)
+
 // Element is data
 type Element int
 
 // Queue is Queue Data
 type Queue struct {
-	e Element
+	e    Element
 	next *Queue
 }
+
 // QueueLink 是队列
 type QueueLink struct {
-	front,rear *Queue
-	length int
+	front, rear *Queue
+	length      int
 }
+
 //New 新建队列
-func New() *QueueLink{
-	q:= Queue{e:Element(0)}
-	return &QueueLink{front:&q,rear:&q,length:0}
+func New() *QueueLink {
+	q := Queue{e: Element(0)}
+	return &QueueLink{front: &q, rear: &q, length: 0}
 }
 
 //Clear 清空队列
@@ -26,33 +32,53 @@ func (q *QueueLink) Clear() bool {
 }
 
 //Destroy 销毁队列
-func (q *QueueLink) Destroy() bool{
+func (q *QueueLink) Destroy() bool {
 	return true
 }
 
 //Empty 队列是否为空
-func (q *QueueLink) Empty() bool{
-	return true
+func (q *QueueLink) Empty() bool {
+	if q.front == q.rear {
+		return true
+	}
+	return false
 }
 
 //Head 获取队头元素
-func (q *QueueLink) Head() (e Element, err error){
+func (q *QueueLink) Head() (e Element, err error) {
+	if q.front == q.rear {
+		err = fmt.Errorf("queue is empty")
+		return
+	}
+	e = q.front.next.e
 	return
 }
+
 //In : 进队列
-func (q *QueueLink) In(e Element) (err error){
-	node := Queue{e:e}
-	q.rear = &node
+func (q *QueueLink) In(e Element) (err error) {
+	node := Queue{e: e}
+	q.rear.next = &node
+	q.rear = q.rear.next
 	q.length++
 	return
 }
 
 //Out : 出队列
-func (q *QueueLink) Out() (e Element, err error){
+func (q *QueueLink) Out() (e Element, err error) {
+	if q.front == q.rear {
+		err = fmt.Errorf("queue is empty")
+		return
+	}
+	e = q.front.next.e
+	q.front = q.front.next.next
+	q.length--
+	if q.length == 0 {
+		q.rear = q.front
+	}
 	return
 }
-//Length 获取队列长房
-func (q *QueueLink) Length() int{
-	return 0
-}
 
+//Length 获取队列长度
+func (q *QueueLink) Length() int {
+	return q.length
+}
--- queue/chain/queue_test.go
+++ queue/chain/queue_test.go
@@ -3,10 +3,44 @@ package chain
 import (
 	"testing"
 )
-func TestNew(t *testing.T){
-	q:=New()
-	if q.length != 0{
+
+func TestNew(t *testing.T) {
+	q := New()
+	if q.length != 0 {
 		t.Error("front should nil")
 	}
 }
 
+func TestEmpty(t *testing.T) {
+	q := New()
+	if !q.Empty() {
+		t.Error("error should empty")
+	}
+}
+
+func TestIn(t *testing.T) {
+	q := New()
+	q.In(Element(1))
+	if q.front.next.e != 1 {
+		t.Errorf("element is %d", q.front.next.e)
+	}
+}
+
+func TestOut(t *testing.T) {
+	q := New()
+	q.In(Element(1))
+	if q.front.next.e != 1 {
+		t.Errorf("element is %d", q.front.next.e)
+	}
+	e, err := q.Out()
+	if err != nil {
+		t.Error(err)
+	}
+	if e != 1 {
+		t.Error("e is not 1")
+	}
+
+	if q.Length() != 0 {
+		t.Error("queue should empty")
+	}
+}
