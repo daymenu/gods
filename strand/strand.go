b0f7b5144256a606ce102a955d8b46009edafa2e
韩剑 <18335831710@163.com>
2019-06-16 22:11:44 +0800

    串

---
+++ strand/strand.go
@@ -0,0 +1,5 @@
+package strand
+
+func strstr() {
+
+}
--- sum/gs.go
+++ sum/gs.go
@@ -1,6 +1,6 @@
 package sum
 
-// 高斯求和
+// GsSum 高斯求和
 func GsSum(start int, end int) (s int) {
 	s = (start + end) * (end - start + 1) / 2
 	return s
