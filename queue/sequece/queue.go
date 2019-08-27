7ad3db92052291e62a31978f727926881d1f9ff5
daymenu <hanjian2018@126.com>
2019-08-27 14:35:04 +0800

    队列和循环队列

--- queue/sequece/queue.go
+++ queue/sequece/queue.go
@@ -1,45 +1,58 @@
 package sequece
 
-import "fmt"
+import "errors"
 
-const maxSize = 100
+const (
+	// MaxSize 最大长度
+	MaxSize = 100
+)
 
-//SqQueue 结构体定义
-type SqQueue struct {
-	data  [maxSize]interface{}
+var (
+	//ErrEmpty 队列为错误定义
+	ErrEmpty = errors.New("queue is empty")
+	//ErrFull 队列为错误定义
+	ErrFull = errors.New("queue is full")
+)
+
+// SqQuece 队列结构体
+type SqQuece struct {
+	el    []interface{}
 	front int
 	rear  int
 }
 
-//New 新建空队列
-func New() *SqQueue {
-	return &SqQueue{
-		front: 0,
-		rear:  0,
-	}
-}
-
-// Length 队列长度
-func (q *SqQueue) Length() interface{} {
-	return (q.rear - q.front + maxSize) % maxSize
+// New 新建队列
+func New() *SqQuece {
+	return new(SqQuece)
 }
 
 // Enqueue 入队
-func (q *SqQueue) Enqueue(e interface{}) error {
-	if (q.rear+1)%maxSize == q.front {
-		return fmt.Errorf("quque is full")
+func (q *SqQuece) Enqueue(el interface{}) error {
+	if q.rear == MaxSize && q.front != 1 {
+		// 数据移动
+		moveNum := MaxSize - q.front - 1
+		for i := 1; i <= moveNum; i++ {
+			q.el[i] = q.el[q.front]
+		}
+	} else {
+		return ErrFull
 	}
-	q.data[q.rear] = e
-	q.rear = (q.rear + 1) % maxSize
+	q.el[q.rear] = el
+	q.rear++
 	return nil
 }
 
 // Dequeue 出队
-func (q *SqQueue) Dequeue() (e interface{}, err error) {
+func (q *SqQuece) Dequeue() (interface{}, error) {
 	if q.rear == q.front {
-		return e, fmt.Errorf("quque is empty")
+		return nil, ErrEmpty
 	}
-	e = q.data[q.front]
-	q.front = (q.front + 1) % maxSize
-	return e, nil
+	el := q.el[q.front]
+	q.front++
+	return el, nil
+}
+
+// Length 长度
+func (q *SqQuece) Length() int {
+	return q.rear - q.front
 }
