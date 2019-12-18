e1c2617e090ab7eab4932bbac7ded19e98dc104a
hanjian011 <hanjian011@ke.com>
2019-12-18 21:11:11 +0800

    skiplist

--- skiplist/skiplist.go
+++ skiplist/skiplist.go
@@ -1,17 +1,20 @@
 package skiplist
 
-import "math"
+import (
+	"math"
+	"math/rand"
+	"time"
+)
 
 // MaxLevel 跳表的最大层级
 const MaxLevel = 16
 
 // SkipNode 跳表中的节点
 type SkipNode struct {
-	v     interface{}
-	level int
-	scroe int
-	right *SkipNode
-	down  *SkipNode
+	v        interface{}
+	level    int
+	scroe    int
+	forwords []*SkipNode
 }
 
 // SkipList 跳表结构体
@@ -23,7 +26,7 @@ type SkipList struct {
 
 // NewSkipNode 新建跳表节点
 func NewSkipNode(v interface{}) *SkipNode {
-	return &SkipNode{v, MaxLevel, math.MinInt32, nil, nil}
+	return &SkipNode{v, MaxLevel, math.MinInt32, nil}
 }
 
 // NewSkipList 建立列表
@@ -39,4 +42,13 @@ func (s *SkipList) Level() int {
 // Length 跳表长度
 func (s *SkipList) Length() int {
 	return s.length
+}
+
+func randomLevel() int {
+	level := 1
+	rand.Seed(time.Now().UnixNano())
+	for rand.Intn(MaxLevel) < 8 && level < MaxLevel {
+		level++
+	}
+	return level
 }
---
+++ skiplist/skiplist_test.go
@@ -0,0 +1,33 @@
+package skiplist
+
+import (
+	"sort"
+	"testing"
+)
+
+/**
+测试随机函数的概率
+*/
+func TestRandomLevel(t *testing.T) {
+	randoms := make(map[int]int)
+	randomTime := 10000000
+	for i := 0; i < randomTime; i++ {
+		randoms[randomLevel()]++
+	}
+	randKeys := getMapKeys(randoms)
+	for _, index := range randKeys {
+		t.Logf("%d : %f", index, float64(randoms[index])/float64(randomTime))
+	}
+	t.Error()
+}
+
+func getMapKeys(m map[int]int) []int {
+	keys := make([]int, len(m))
+	j := 0
+	for key := range m {
+		keys[j] = key
+		j++
+	}
+	sort.Ints(keys)
+	return keys
+}
