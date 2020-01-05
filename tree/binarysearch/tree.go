e5452c7b09dbf3c86bcc654d908a930e06430470
hanjian <hanjian2018@126.com>
2020-01-06 00:03:15 +0800

    tree

--- tree/binarysearch/tree.go
+++ tree/binarysearch/tree.go
@@ -1,1 +1,116 @@
 package binarysearch
+
+import "fmt"
+
+// 实现二叉查找树
+
+// Comparer 可比较的数据
+type Comparer interface {
+	Compare(data interface{}) int
+}
+
+// Node tree node
+type Node struct {
+	data  Comparer
+	left  *Node
+	right *Node
+}
+
+// Tree tree struct
+type Tree struct {
+	root   *Node
+	length int
+}
+
+// New new tree
+func New() *Tree {
+	return &Tree{
+		root:   nil,
+		length: 0,
+	}
+}
+
+// Insert 在树插入一个值
+func (t *Tree) Insert(data Comparer) error {
+	if t.root == nil {
+		t.root = &Node{data: data}
+		t.length++
+		return nil
+	}
+	p := t.root
+	for p != nil {
+		if p.data.Compare(data) < 0 {
+			if p.left == nil {
+				p.left = &Node{data: data}
+				t.length++
+				return nil
+			}
+			p = p.left
+		} else {
+			if p.right == nil {
+				p.right = &Node{data: data}
+				t.length++
+				return nil
+			}
+			p = p.right
+		}
+	}
+	return nil
+}
+
+// PreOrder 前序遍历
+func (t *Tree) PreOrder() {
+	p := t.root
+	if p == nil {
+		return
+	}
+	p.preOrder()
+}
+
+func (n *Node) preOrder() {
+	if n.left != nil {
+		n.left.preOrder()
+	}
+	fmt.Println(n.data)
+	if n.right != nil {
+		n.right.preOrder()
+	}
+}
+
+// PostOrder 后序遍历
+func (t *Tree) PostOrder() {
+	p := t.root
+	if p == nil {
+		return
+	}
+	p.postOrder()
+}
+
+func (n *Node) postOrder() {
+	if n.right != nil {
+		n.right.preOrder()
+	}
+	fmt.Println(n.data)
+	if n.left != nil {
+		n.left.preOrder()
+	}
+}
+
+// InOrder 中序遍历
+func (t *Tree) InOrder() {
+	p := t.root
+	if p == nil {
+		return
+	}
+	p.inOrder()
+}
+
+func (n *Node) inOrder() {
+	fmt.Println(n.data)
+	if n.left != nil {
+		n.left.preOrder()
+	}
+	if n.right != nil {
+		n.right.preOrder()
+	}
+}
---
+++ tree/binarysearch/tree_test.go
@@ -0,0 +1,31 @@
+package binarysearch
+
+import (
+	"strings"
+	"testing"
+)
+
+type Order struct {
+	ID       string
+	GoodName string
+}
+
+func (o *Order) Compare(data interface{}) int {
+	d := data.(*Order)
+	return strings.Compare(o.ID, d.ID)
+}
+func TestTree(t *testing.T) {
+	tree := New()
+	o := &Order{ID: "J0001", GoodName: "可乐"}
+	o1 := &Order{ID: "J0002", GoodName: "雪碧"}
+	o2 := &Order{ID: "J0003", GoodName: "茶"}
+	o3 := &Order{ID: "J0004", GoodName: "内裤"}
+	tree.Insert(o1)
+	tree.Insert(o)
+	tree.Insert(o2)
+	tree.Insert(o3)
+	tree.PreOrder()
+	// tree.InOrder()
+	// tree.PostOrder()
+	t.Error()
+}
